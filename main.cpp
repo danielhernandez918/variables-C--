ab954e19318121d0d79a2686eb31e2ae0ec2951d
Daniel Hernandez <danielhernandez918@gmail.com>
2022-11-07 15:00:03 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main () 
+{
+    string charName = "John";
+    int characterAge;
+    characterAge = 35;
+    cout << "There was a man named " << charName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << charName << endl;
+    cout << "But did not like being " << characterAge << endl;
+    return 0;
+}
